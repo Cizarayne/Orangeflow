c8dab744cc0e6da3d8ec1bc3c6c7762683fb65ae
cizarayne <promiseosemhen@gmail.com>
2026-07-12 17:48:07 +0100

    few changes

--- client/src/api/Axios.js
+++ client/src/api/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const connector = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:9000/api",
+  baseURL: "https://orangeflow-eta.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
